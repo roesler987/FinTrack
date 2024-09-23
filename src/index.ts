0e1e857b18ab20983d14b26900477cf9a8c70fb5
jean <jkroesler@hotmail.com>
2024-09-23 15:32:31 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("adajdasj")
+import express from "express";
+import { config } from "dotenv";
+
+const app = express()
+
+const port = process.env.PORT || 8000;
+
+app.get ("/", (req, res) =>{
+    res.send("ola seus filhos da puta")
+})
+
+app.listen(port, () => console.log(`listening on port ${port}!`) );
