5325ab7ae205be33c7f6afcbc2af520f5d70fc96
jean <jkroesler@hotmail.com>
2024-09-25 17:23:18 -0300

    config database

---
+++ src/database/mongo.ts
@@ -0,0 +1,21 @@
+import {MongoClient as Mongo,Db } from 'mongodb'
+
+export const MongoCliente ={
+    cliente: undefined as unknown as Mongo,
+    db: undefined as unknown as Db,
+
+
+    async connect(): Promise <void>{
+        const url = process.env.MONGODB_URL || "localhost:8000";
+        const username = process.env.MONGODB_USERNAME;
+        const password = process.env.MONGODB_PASSWORD;
+
+        const cliente = new Mongo(url, {auth: { username, password } });
+        const db = cliente.db("users-db");
+
+        this.cliente = cliente;
+        this.db =db;
+
+        console.log("connect to mongoDB")
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,28 @@
 import express from "express";
 import { config } from "dotenv";
-import { MongoGetUsersRepository } from './repositories/get-users/mongo-get-users';
-import { GetUsersController } from './controllers/get-users/get-users';
+import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
+import { GetUsersController } from "./controllers/get-users/get-users";
+import { MongoCliente } from "./database/mongo";
 
-const app = express()
+const main = async () => {
+    config();
+    const app = express();
 
-const port = process.env.PORT || 8000;
+    const port = process.env.PORT || 8000;
 
-app.get ("/users", async (req, res) =>{
-    const mongoGetUsersRepository = new MongoGetUsersRepository();
+    await MongoCliente.connect();
 
-    const getUsersController = new GetUsersController(mongoGetUsersRepository);
+    app.get("/users", async (req, res) => {
+        const mongoGetUsersRepository = new MongoGetUsersRepository();
 
-    const { body, statusCode } = await getUsersController.handle();
+        const getUsersController = new GetUsersController(mongoGetUsersRepository);
 
-    res.send(body).status(statusCode);
-})
+        const { body, statusCode } = await getUsersController.handle();
 
-app.listen(port, () => console.log(`listening on port ${port}!`) );
+        res.send(body).status(statusCode);
+    });
+
+    app.listen(port, () => console.log(`listening on port ${port}!`));
+};
+
+main();
