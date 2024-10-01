ed3321ee0076fcb2b737f91e22d5d0ee60248dc5
jean <jkroesler@hotmail.com>
2024-10-01 08:02:01 -0300

    Resolved user authentication issue in database connection

--- src/controllers/get-users/get-users.ts
+++ src/controllers/get-users/get-users.ts
@@ -1,23 +1,23 @@
 import { IGetUsersController, IGetUsersRepository } from './procols';
 
-export class GetUsersController implements IGetUsersController{
-    constructor(private readonly getUserRepository: IGetUsersRepository){}
+export class GetUsersController implements IGetUsersController {
+    constructor(private readonly getUserRepository: IGetUsersRepository) {}
 
-   async handle() {
-    try{
-        const users = await this.getUserRepository.getUsers()
+    async handle() {
+        try {
+            const users = await this.getUserRepository.getUsers();
 
-        return{
-            statusCode: 200,
-            body: users,
-        };
-    } catch(error){
+            return {
+                statusCode: 200,
+                body: users,
+            };
+        } catch (error) {
+            console.log('Error fetching users:', error);
 
-        return{
-            statusCode:500,
-            body: "Something went wrong"
-        };
+            return {
+                statusCode: 500,
+                body: 'Something went wrong',
+            };
+        }
     }
-
-   }
 }
