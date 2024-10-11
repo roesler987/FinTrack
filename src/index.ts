df483f705117916091fdd10fc3aeb4256650e114
jean <jkroesler@hotmail.com>
2024-10-11 14:09:23 -0300

    update user controller with validations

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,16 @@ import { config } from "dotenv";
 import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
 import { GetUsersController } from "./controllers/get-users/get-users";
 import { MongoClient } from "./database/mongo";
-import { MongoCreateUserRepository } from './repositories/create-user/mongo-create-user';
-import { CreateUserController } from './controllers/create-user/create-user';
+import { MongoCreateUserRepository } from "./repositories/create-user/mongo-create-user";
+import { CreateUserController } from "./controllers/create-user/create-user";
+import { UpdateUserController } from "./controllers/update-user/update-user";
+import { MongoUpdateUserRepository } from "./repositories/update-user/mongo-update-user";
 
 const main = async () => {
   config();
   const app = express();
-  
-  app.use(express.json())
+
+  app.use(express.json());
 
   const port = process.env.PORT || 8000;
 
@@ -32,10 +34,24 @@ const main = async () => {
       mongoCreateUserRepository
     );
 
-    const {body, statusCode} = await createUserController.handle({
+    const { body, statusCode } = await createUserController.handle({
       body: req.body,
-    })
+    });
 
+    res.status(statusCode).send(body);
+  });
+
+  app.patch("/users/:id", async (req, res) => {
+    const mongoUpdateUserRepository = new MongoUpdateUserRepository();
+
+    const updateUserController = new UpdateUserController(
+      mongoUpdateUserRepository
+    );
+
+    const { body, statusCode } = await updateUserController.handle({
+      body: req.body,
+      params: req.params,
+    });
     res.status(statusCode).send(body);
   });
 
